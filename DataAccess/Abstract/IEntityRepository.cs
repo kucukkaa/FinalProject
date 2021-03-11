d4dc7b33f73a61adbdb131381b6489de4a0f456a
Alierk KÜÇÜK <Alierk KK@DESKTOP-OOU3I1N>
2021-03-11 17:47:45 +0300

    11.03.2021

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+
+    //generic constraint = kısıt
+    
+    
+    public interface IEntityRepository<T> where T:class,IEntity,new()   //referans tip olabilir demek yalnızca class anlamına gelmez, IEntity olabilir ya da implemente eden bir nesne olabilir. ayrıca yalnızca newlenebilir nesne eklenebilir. bu sayede IEntity de elenmiş olur.
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    
+    //context : db tabloları ile proje classlarını bağlamak 
+    public class NorthwindContext : DbContext //entityframework içinde gelen bir base sınıf
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server = (localDb)\mssqllocaldb;Database=Northwind; Trusted_Connection=true");
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
