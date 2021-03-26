5acdc2b08d7c3b64e7c9a4fa30fe7727c67360f6
Alierk KÜÇÜK <Alierk KK@DESKTOP-OOU3I1N>
2021-03-26 16:19:14 +0300

    26.03.2021

--- WepAPI/Controllers/ProductsController.cs
+++ WepAPI/Controllers/ProductsController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace WepAPI.Controllers
@@ -29,6 +30,8 @@ namespace WepAPI.Controllers
         [HttpGet("getall")]
         public IActionResult GetAll()
         {
+            Thread.Sleep(5000);
+            
             //dependency chain --
             
             var result = _productService.GetAll();
--- WepAPI/Startup.cs
+++ WepAPI/Startup.cs
@@ -43,7 +43,7 @@ namespace WepAPI
             services.AddControllers();
             //services.AddSingleton<IProductService , ProductManager>();
             //services.AddSingleton<IProductDal, EfProductDal>();
-            
+            services.AddCors();
 
             var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
 
@@ -72,6 +72,8 @@ namespace WepAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+            
+            app.UseCors(builder => builder.WithOrigins("http://localhost:4200").AllowAnyHeader());
 
             app.UseHttpsRedirection();
 
